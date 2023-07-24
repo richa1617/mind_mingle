025eb50b00e3433f4c8739d32310324f774c7c20
Richa Mahajan <richamahajan1617@gmail.com>
2023-07-24 12:18:31 +0200

    Intial setup for prisma

---
+++ database/queries.ts
@@ -0,0 +1,45 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const createMany = async () => {
+  // await prisma.student.createMany({
+  //   data: [
+  //     {
+  //       name: "Albert",
+  //       classNumber: 1,
+  //     },
+  //     {
+  //       name: "Richa",
+  //       classNumber: 2,
+  //     },
+  //     {
+  //       name: "Bla",
+  //       classNumber: 5,
+  //     },
+  //     {
+  //       name: "Jp",
+  //       classNumber: 1,
+  //     },
+  //   ],
+  // });
+};
+
+//createMany();
+
+// const allStudent = async () => {
+//   const students = await prisma.student.findMany();
+//   console.log(students);
+// };
+
+const unique = async () => {
+  const students = await prisma.student.findFirst({
+    where: {
+      classNumber: 2,
+    },
+  });
+
+  console.log(students);
+};
+
+unique();
