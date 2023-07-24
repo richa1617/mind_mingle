dec4dae8ad2fec6e508120fe70597608816984bb
Richa Mahajan <richamahajan1617@gmail.com>
2023-07-24 13:32:00 +0200

    Learned about various query methods

--- database/queries.ts
+++ database/queries.ts
@@ -32,15 +32,67 @@ const createMany = async () => {
 //   console.log(students);
 // };
 
-
-const unique = async () => {
-  const students = await prisma.student.findFirst({
+const queris = async () => {
+  const firstStudent = await prisma.student.findFirst({
     where: {
       classNumber: 2,
     },
   });
+  //console.log(firstStudent);
 
-  console.log(students);
+  // const allStudent = await prisma.student.findMany();
+  // console.log(allStudent);
+
+  const ageMoreThenFour = await prisma.student.findMany({
+    where: {
+      age: {
+        gte: 4,
+      },
+    },
+    select: {
+      name: true,
+    },
+  });
+
+  //console.log(ageMoreThenFour)
+
+  const Id2 = await prisma.student.findUnique({
+    where: {
+      id: 2,
+    },
+  });
+
+  //console.log(Id2);
+
+  const studentClass5 = await prisma.student.findMany({
+    where: {
+      classNumber: 5,
+    },
+  });
+
+  //console.log(studentClass5);;
+
+  const update = await prisma.student.update({
+    where: {
+      id: 3,
+    },
+    data: {
+      classNumber: 3,
+    },
+  });
+
+  //console.log(update);
+  const allStudents = await prisma.student.findMany();
+  console.log(allStudents.length);
+ 
+  await prisma.student.delete({
+    where: {
+      id: 2,
+    },
+  });
+  // Get the amount of students after the query
+  const allStudentsAfterDelete = await prisma.student.findMany();
+  console.log(allStudentsAfterDelete.length);
 };
 
-unique();
+queris();
